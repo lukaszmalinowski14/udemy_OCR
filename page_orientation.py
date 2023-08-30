46a31b0bafc9209fc4288d6d7b58a7e09fd66d3a
Łukasz Malinowski <lukaszmalinowski14@gmail.com>
2023-08-30 22:33:56 +0200

    "30_08_2023"

--- page_orientation.py
+++ page_orientation.py
@@ -4,6 +4,10 @@ import cv2 as cv2
 from PIL import Image
 import matplotlib.pyplot as plt
 from pytesseract import Output
+from PIL import ImageFont, ImageDraw, Image
+import re  # regular expressions
+
+font = 'calibri.ttf'
 
 # your path may be different
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
@@ -27,6 +31,24 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesserac
 ##############################################################################
 # DEF
 ############
+
+
+def show_resized(rgb):
+    resize = ResizeWithAspectRatio(rgb, width=600)
+    cv2.imshow("regex", resize)
+    showInMovedWindow('regex', resize, 0, 200)
+
+    cv2.waitKey(0)  # waits until a key is pressed
+    cv2.destroyAllWindows()  # destroys the window showing image
+
+
+def write_text(text, x, y, img, font, font_size=32):
+    font = ImageFont.truetype(font, font_size)
+    img_pil = Image.fromarray(img)
+    draw = ImageDraw.Draw(img_pil)
+    draw.text((x, y-font_size), text, font=font)
+    img = np.array(img_pil)
+    return img
 
 
 def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -59,6 +81,35 @@ def showInMovedWindow(winname, img, x, y):
     cv2.namedWindow(winname)        # Create a named window
     cv2.moveWindow(winname, x, y)   # Move it to (x,y)
     cv2.imshow(winname, img)
+
+
+def mark_bounding_box(rgb, lang, font_size=32, pattern='(?s).*'):
+    min_confidence = 40
+    img_copy = rgb.copy()
+    for i in range(0, len(result['text'])):
+        confidence = int(result['conf'][i])
+        if confidence > min_confidence:
+            text = result['text'][i]
+            if not text.isspace() and len(text) > 0:
+                if pattern != '':
+                    if re.match(pattern, text):
+                        x, y, img = bounding_box(
+                            result, img_copy, i, (0, 0, 255))
+                        if lang == 'eng':
+                            cv2.putText(img_copy, text, (x, y-10),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 0, 255))
+                        elif lang == 'por':
+                            img_copy = write_text(
+                                text, x, y, img_copy, font, font_size)
+                    else:
+                        x, y, img_copy = bounding_box(result, img_copy, i)
+                        if lang == 'eng':
+                            cv2.putText(img_copy, text, (x, y-10),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 0, 255))
+                        elif lang == 'por':
+                            img_copy = write_text(
+                                text, x, y, img_copy, font, font_size)
+    return img_copy
 #######################################################################
 # MAIN
 #######################################################################
@@ -80,17 +131,18 @@ result = pytesseract.image_to_data(
 
 print(result)
 
-min_confidence = 40
-img_copy = rgb.copy()
-for i in range(0, len(result['text'])):
-    confidence = int(result['conf'][i])
-    if confidence > min_confidence:
-        # print(confidence)
-        x, y, img = bounding_box(result, img_copy, i)
-        # print(x, y)
-        text = result['text'][i]
-        cv2.putText(img_copy, text, (x, y-10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 0, 255))
+# min_confidence = 40
+# img_copy = rgb.copy()
+# for i in range(0, len(result['text'])):
+#     confidence = int(result['conf'][i])
+#     if confidence > min_confidence:
+#         # print(confidence)
+#         x, y, img = bounding_box(result, img_copy, i)
+#         # print(x, y)
+#         text = result['text'][i]
+#         cv2.putText(img_copy, text, (x, y-10),
+#                     cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 0, 255))
+img_copy = mark_bounding_box(rgb, 'eng')
 resize = ResizeWithAspectRatio(img_copy, width=600)  # Resize by width OR
 cv2.imshow("test", resize)
 showInMovedWindow('test', resize, 0, 200)
@@ -98,8 +150,69 @@ showInMovedWindow('test', resize, 0, 200)
 cv2.waitKey(0)  # waits until a key is pressed
 cv2.destroyAllWindows()  # destroys the window showing image
 
-print("bye")
 
+# plik 2 portugalski
+img = cv2.imread('test02-02.jpg')
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+resize = ResizeWithAspectRatio(rgb, width=600)
+cv2.imshow("result", resize)
+
+cv2.waitKey(0)  # waits until a key is pressed
+cv2.destroyAllWindows()  # destroys the window showing image
+
+config_tesseract = '--tessdata-dir tessdata'
+result = pytesseract.image_to_data(
+    rgb, config=config_tesseract, lang='por', output_type=Output.DICT)
+print(result)
+
+img_with_box = mark_bounding_box(rgb, 'por')
+resize = ResizeWithAspectRatio(img_with_box, width=600)  # Resize by width OR
+cv2.imshow("test", resize)
+showInMovedWindow('test', resize, 0, 200)
+
+cv2.waitKey(0)  # waits until a key is pressed
+cv2.destroyAllWindows()  # destroys the window showing image
+
+# TODO: REGEX
+# regex
+date_pattern = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
+
+img = cv2.imread('table_test.jpg')
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+resize = ResizeWithAspectRatio(rgb, width=600)
+cv2.imshow("regex", resize)
+showInMovedWindow('regex', resize, 0, 200)
+
+cv2.waitKey(0)  # waits until a key is pressed
+cv2.destroyAllWindows()  # destroys the window showing image
+
+result = pytesseract.image_to_data(
+    rgb, config=config_tesseract, lang='por', output_type=Output.DICT)
+print(result)
+
+img_with_box = mark_bounding_box(rgb, 'por', 20, date_pattern)
+resize = ResizeWithAspectRatio(img_with_box, width=600)  # Resize by width OR
+cv2.imshow("test", resize)
+showInMovedWindow('test', resize, 0, 200)
+
+cv2.waitKey(0)  # waits until a key is pressed
+cv2.destroyAllWindows()  # destroys the window showing image
+
+# regexr.com
+# TODO: NATURAL SCENARIOS
+config_tesseract = '--tessdata-dir tessdata --psm 7'
+
+img = cv2.imread('cup.jpg')
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+show_resized(rgb)
+
+result = pytesseract.image_to_data(
+    rgb, lang='eng', output_type=Output.DICT)
+print(result)
+
+img_with_box = mark_bounding_box(rgb, 'eng', 20, date_pattern)
+show_resized(img_with_box)
 # block_num = Current block number. When Tesseract performs the detections,
 #  it divides the image into several regions, which can vary according to the PSM parameters and also other criteria of the algorithm. Each block is a region
 
@@ -129,3 +242,4 @@ print("bye")
 # width = width of the current detected text block
 
 # word_num = word number (index) within the current block
+print("bye")
